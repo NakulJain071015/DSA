5ce705ae3121723db20b197b7c968a059b4c3d44
Nakul Jain <nakuljain152007@gmail.com>
2026-08-15 12:50:05 +0530

    Time: 0 ms (100%), Space: 10 MB (53.94%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
+bool isAlphaNum(char ch){
+    if((ch >= '0' && ch <= '9') || (tolower(ch) >= 'a' && tolower(ch) <= 'z')){
+        return true;
+    }
+    return false;
+}
     bool isPalindrome(string s) {
-        int l = 0, r = s.length() - 1;
-
-        while (l < r) {
-            while (l < r && !isalnum(s[l]))
-                l++;
-            while (l < r && !isalnum(s[r]))
-                r--;
-
-            if (tolower(s[l]) != tolower(s[r]))
-                return false;
-
-            l++;
-            r--;
+        int st = 0;
+        int end = s.length() - 1;
+        while(st < end){
+            if(!isAlphaNum(s[st])){
+                st++;
+                continue;
+            }
+            if(!isAlphaNum(s[end])){
+                end--;
+                continue;
+            }
+            if(tolower(s[st]) != tolower(s[end])) return false;
+            st++;
+            end--;
         }
         return true;
     }
+
 };
