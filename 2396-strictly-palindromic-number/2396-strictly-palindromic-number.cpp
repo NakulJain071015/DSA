5d93f3385da0cef477f3f59dce6d57c426dbdfac
Nakul Jain <nakuljain152007@gmail.com>
2026-08-22 08:28:41 +0530

    Time: 0 ms (100%), Space: 8.2 MB (5.79%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
@@ -1,6 +1,30 @@
 class Solution {
 public:
+    bool isPalindrome(string& s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            if (s[i] != s[j]) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
     bool isStrictlyPalindromic(int n) {
-        return false;
+        for (int i = 2; i <= n - 2; i++) {
+            string temp = "";
+            int num = n;
+            while(num){
+                temp += to_string(num%i);
+                num = num/i;
+            }
+
+            if(!isPalindrome(temp)){
+                return false;
+            }
+        }
+        return true;
     }
 };
