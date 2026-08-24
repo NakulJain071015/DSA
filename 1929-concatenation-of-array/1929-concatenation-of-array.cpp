2e5a6a72b3b38433c5a3b917b870f7bde1796b66
Nakul Jain <nakuljain152007@gmail.com>
2026-08-24 21:32:01 +0530

    Time: 0 ms (100%), Space: 16.8 MB (59.43%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cpp
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -5,8 +5,6 @@ public:
         vector<int> ans(2*n);
         for(int i = 0; i<n; i++){
             ans[i] = nums[i];
-        }
-        for(int i = 0; i < n; i++){
             ans[i+n] = nums[i];
         }
         
