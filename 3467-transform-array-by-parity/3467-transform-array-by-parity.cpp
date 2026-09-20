611594265f56509ea4ad569280cb6140e7c8fec9
Nakul Jain <nakuljain152007@gmail.com>
2026-09-20 14:33:08 +0530

    Time: 0 ms (100%), Space: 28.2 MB (88.71%) - LeetHub

---
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> transformArray(vector<int>& nums) {
+        vector<int>ans;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] % 2 == 0){
+                ans.push_back(0);
+            }
+            else if(nums[i] % 2 != 0){
+                ans.push_back(1);
+            }
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
