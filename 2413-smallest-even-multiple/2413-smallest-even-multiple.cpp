feb1411704ad70d19e3028eccfc28905e9f02498
Nakul Jain <nakuljain152007@gmail.com>
2026-09-25 20:39:45 +0530

    Time: 0 ms (100%), Space: 8 MB (30.7%) - LeetHub

---
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int smallestEvenMultiple(int n) {
+        int ans;
+        if(n % 2 != 0){
+            return ans = n*2;
+        }
+        else{
+            return n;
+        }
+
+    }
+};
